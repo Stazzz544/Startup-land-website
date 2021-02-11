31db3113fd75ebfc44cacbc50304ebdb587c4d0f
Stas_PC <stazzzdzr@gmail.com>
2021-02-11 22:19:20 +0300

    Burger ready
    Adaptive complite
    Burger ready

--- landing-Startup/JS/script.js
+++ landing-Startup/JS/script.js
@@ -119,4 +119,10 @@ jQuery(document).ready(function($){
 });
 
 
-//Swiper buttom
+//burger
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
